a11ce475708565f7a5b3ad650e3c71c9e8c89525
Crazy_pro <Crazy.pro.noob@gmail.com>
2024-06-08 12:00:00 +0200

    Add repositories

---
+++ src/main/java/alex/klimchuk/petclinic/fauxspring/ModelAndView.java
@@ -0,0 +1,14 @@
+package alex.klimchuk.petclinic.fauxspring;
+
+public class ModelAndView {
+
+    public ModelAndView() {
+    }
+
+    public ModelAndView(String view) {
+    }
+
+    public void addObject(Object o) {
+    }
+
+}
